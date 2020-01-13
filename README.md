f06839ee2acb62d420c6a1308a68f7ce52715c09
Paul Scott <paul.scott@ravelin.com>
2020-01-13 19:17:57 +0000

    Create cookies with a delay.

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ A JavaScript snippet which sets a bunch of cookies so you can see what your
 browser will complain about. Paste this into your browser's console to run:
 
 ```js
-(function(src) { var s = document.createElement('script'); s.src = src; document.body.appendChild(s); })('https://cdn.jsdelivr.net/gh/icio/cookies@master/cookies.js')
+(function(src) {
+    var s = document.createElement('script');
+    s.src = src;
+    document.body.appendChild(s);
+})('https://cdn.jsdelivr.net/gh/icio/cookies@master/cookies.js')
 ```
--- cookies.js
+++ cookies.js
@@ -2,12 +2,23 @@
     var sameSite = ['', 'SameSite=None', 'SameSite=Lax', 'SameSite=Strict'];
     var secure = ['', 'Secure'];
 
+    // Permute the cookies to generate.
+    var cookies = [];
     for (var s = 0; s < sameSite.length; s++) {
         for (var t = 0; t < secure.length; t++) {
             var name = 'cookieTest' + sameSite[s].replace(/=/, '') + secure[t];
             var val = ['ok', 'path=/', sameSite[s], secure[t]].join(';').replace(/;+/g, ';').replace(/;+$/g, '');
-            console.info(name+"="+val);
-            document.cookie = name + '=' + val;
+            cookies.push(name + '=' + val);
         }
     }
+
+    // Set the cookies with an interval between each, to allow the browser
+    // cookie warning to trigger between each setting.
+    var interval;
+    interval = setInterval(function() {
+        var c = cookies.shift();
+        console.info(c);
+        // document.cookie = c;
+        if (cookies.length == 0) clearInterval(interval);
+    }, 100);
 })();
