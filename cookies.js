8826ceb0a3b0a731f002c3573b70e1d666d5b730
Paul Scott <paul.scott@ravelin.com>
2020-01-13 19:11:09 +0000

    Permute various Secure/SameSite cookie options.

--- cookies.js
+++ cookies.js
@@ -1,1 +1,13 @@
-document.cookie = 'cookieDefault=ok';
+(function() {
+    var sameSite = ['', 'SameSite=None', 'SameSite=Lax', 'SameSite=Strict'];
+    var secure = ['', 'Secure'];
+
+    for (var s = 0; s < sameSite.length; s++) {
+        for (var t = 0; t < secure.length; t++) {
+            var name = 'cookieTest' + sameSite[s].replace(/=/, '') + secure[t];
+            var val = ['ok', 'path=/', sameSite[s], secure[t]].join(';').replace(/;+/g, ';').replace(/;+$/g, '');
+            console.info(name+"="+val);
+            document.cookie = name + '=' + val;
+        }
+    }
+})();
