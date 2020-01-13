79e4b7083ad85491443aec2aca2ce58fdfdbb930
Paul Scott <paul.scott@ravelin.com>
2020-01-13 19:29:37 +0000

    Async indirect.

--- indirect.js
+++ indirect.js
@@ -1,5 +1,6 @@
 (function(src) {
     var s = document.createElement('script');
     s.src = src;
+    s.async = true;
     document.body.appendChild(s);
 })('https://cdn.jsdelivr.net/gh/icio/cookies/cookies.js');
